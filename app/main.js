4514585813275ef301f3c530f968695fe3811930
Patrick Mulder <mulder.patrick@gmail.com>
2014-03-18 22:37:55 +0100

    Extract router to app

--- app/main.js
+++ app/main.js
@@ -9,11 +9,33 @@ var moviesCollection = new Movies();
 var MovieView = require('views/movieView');
 var MoviesView = require('views/moviesView');
 
+var MoviesRouter = Backbone.Router.extend({
+
+  routes: {
+    "movies/:id": "showMovie",
+    "": "showAll",
+  },
+
+  showAll: function() {
+    var movies = new app.views.Movies({collection: app.collections.movies});
+    movies.render();
+  },
+
+  showMovie: function(id) {
+    var movie = app.collections.movies.get(id);
+    var movieView = new app.views.Movie({el: "#details", model: movie});
+    movieView.render();
+  }
+
+});
+
+
 
 module.exports = {
     $: $,
     _: _,
     Backbone: Backbone,
+    Router: MoviesRouter,
     collections: {
       movies: moviesCollection
     },
